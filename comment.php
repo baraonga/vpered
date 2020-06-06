013f16b3245a747a3733c65ff380e405b6a2001e
baraonga <63958828+baraonga@users.noreply.github.com>
2020-06-06 17:40:45 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -9,12 +9,3 @@
   $mysqli->query("INSERT INTO `comments` (`name`, `page_id`, `text_comment`) VALUES ('$name', '$page_id', '$text_comment')");// Добавляем комментарий в таблицу
   header("Location: ".$_SERVER["HTTP_REFERER"]);// Делаем реридект обратно
 ?>
-<?php
-  $page_id = 150;// Уникальный идентификатор страницы (статьи или поста)
-  $mysqli = new mysqli("localhost", "root", "", "db");// Подключается к базе данных
-  $result_set = $mysqli->query("SELECT * FROM `comments` WHERE `page_id`='$page_id'"); //Вытаскиваем все комментарии для данной страницы
-  while ($row = $result_set->fetch_assoc()) {
-    print_r($row); //Вывод комментариев
-    echo "<br />";
-  }
-?>
